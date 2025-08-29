cf8bc6d52e7e47237786d871a863efaaa05ebfc5
Yuvraj <yuvrajbhatiofficial@gmail.com>
2025-08-30 03:10:53 +0530

    setup analytics

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
     
     <Component {...pageProps} />;
    <SpeedInsights/>
+   <Analytics/>
     </>
   );
 }
